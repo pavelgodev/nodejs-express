a8ef2a543e5bc6a424ae318d42c954098d9d7688
Pavel Moiseenko <pavel.moiseenko@hyperion-tech.co>
2023-04-08 15:21:29 +0300

    EXPRESS Передача динамического параметра в url

--- routes/courses.js
+++ routes/courses.js
@@ -12,4 +12,15 @@ router.get('/', async (req, res) => {
     })
 })
 
+
+router.get('/:id', async (req, res) => {
+    const course = await Course.getById(req.params.id)
+
+    res.render("course", {
+        layout: 'empty',
+        title: `Курс ${course.title}`,
+        course
+    })
+})
+
 module.exports = router
