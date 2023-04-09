76bc30d438cf8808ef97499b243408d0d07007f7
Pavel Moiseenko <pavel.moiseenko@hyperion-tech.co>
2023-04-09 12:37:55 +0300

    EXPRESS Редактирование курса

--- routes/courses.js
+++ routes/courses.js
@@ -11,8 +11,6 @@ router.get('/', async (req, res) => {
         courses
     })
 })
-
-
 router.get('/:id', async (req, res) => {
     const course = await Course.getById(req.params.id)
 
@@ -22,5 +20,24 @@ router.get('/:id', async (req, res) => {
         course
     })
 })
+
+router.get('/:id/edit', async (req, res)=>{
+    if(!req.query.allow){
+        return res.redirect('/')
+    }
+
+    const course = await Course.getById(req.params.id)
+
+    res.render('course-edit', {
+        title: `Редактировать ${course.title}`,
+        course
+    })
+})
+
+router.post("/edit", async (req, res)=>{
+    await Course.update(req.body)
+    res.redirect('/courses')
+})
+
 
 module.exports = router
